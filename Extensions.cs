53c98b01c56d7ca9e81744788158d8c76d404c91
Cat <catalin@bullyentertainment.com>
2024-11-19 13:07:21 +0300

    Init IdleNumber

---
+++ Extensions.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEngine;
+
+namespace IdleNumbers
+{
+public static class Extensions
+{
+    public static IdleNumber Addition(IdleNumber numberA, IdleNumber numberB)
+    {
+        numberA.Value += numberB.RoundToTargetValue(numberA.Lvl);
+        numberA.CheckAndRoundIdleNumber();
+
+        return numberA;
+    }
+
+    public static IdleNumber Multiply(IdleNumber numberA, IdleNumber numberB)
+    {
+        numberA.Value *= numberB.Value;
+        numberA.Lvl += numberB.Lvl;
+        numberA.CheckAndRoundIdleNumber();
+
+        return numberA;
+    }
+
+    public static IdleNumber Subtract(IdleNumber numberA, IdleNumber numberB)
+    {
+        numberA.Value -= numberB.RoundToTargetValue(numberA.Lvl);
+        numberA.CheckAndRoundIdleNumber();
+
+        return numberA;
+    }
+
+    public static IdleNumber Divide(IdleNumber numberA, IdleNumber numberB)
+    {
+        numberA.Value /= numberB.Value;
+        numberA.Lvl -= numberB.Lvl;
+        numberA.CheckAndRoundIdleNumber();
+
+        return numberA;
+    }
+    
+
+    /// <summary>
+    /// Round number to certain value (M, B, T, AA, AB) if number is too big or too small
+    /// </summary>
+    public static void CheckAndRoundIdleNumber(this ref IdleNumber idleNumber)
+    {
+        bool isNegative = idleNumber.Value < 0;
+
+        idleNumber.Value = Math.Abs(idleNumber.Value);
+
+        if (idleNumber.Value >= 1000)
+        {
+            while (idleNumber.Value >= 1000)
+            {
+                if (idleNumber.Lvl < ConstantIdleNumber.LVL.Length - 1)
+                {
+                    idleNumber.Value /= 1000;
+                    idleNumber.Lvl++;
+                }
+                else
+                {
+                    idleNumber.Value = Math.Clamp(idleNumber.Value, 0, 999);
+                    break;
+                }
+            }
+        }
+        else if (idleNumber.Value < 1 && idleNumber.Value > 0)
+        {
+            if (idleNumber.Lvl > 0)
+                while (idleNumber.Value < 1)
+                {
+                    idleNumber.Value *= 1000;
+                    idleNumber.Lvl--;
+                }
+            else
+                idleNumber.Reset();
+        }
+        else if (idleNumber.Value == 0)
+        {
+            idleNumber.Reset();
+        }
+
+        idleNumber.Value = Math.Round(idleNumber.Value, Math.Clamp(3 * idleNumber.Lvl, 0, 15));
+
+        if (isNegative)
+            idleNumber.Value *= -1;
+    }
+
+    /// <summary>
+    /// Check if currency amount is bigger or equal to price
+    /// </summary>
+    public static bool IsEnough(this IdleNumber currency, IdleNumber price)
+    {
+        if (currency.Lvl == price.Lvl)
+            return currency.Value >= price.Value;
+
+        return currency.Lvl > price.Lvl;
+    }
+
+    public static double RoundToTargetValue(this IdleNumber idleNumber, int targetIdleNumberLvl)
+    {
+        var lvlDiff = idleNumber.Lvl - targetIdleNumberLvl;
+
+        if (lvlDiff != 0)
+            idleNumber.Value *= Math.Pow(1000, lvlDiff);
+
+        return targetIdleNumberLvl == 0
+            ? idleNumber.Value
+            : Math.Round(idleNumber.Value, Math.Clamp(3 * Math.Abs(lvlDiff), 0, 15));
+    }
+
+    public static double RoundToTargetValue(this double number, int targetIdleNumberLvl)
+    {
+        var lvlDiff = (int)Math.Floor(number / 1000) - targetIdleNumberLvl;
+
+        if (lvlDiff != 0)
+            number *= Mathf.Pow(1000, lvlDiff);
+
+        return targetIdleNumberLvl == 0
+            ? number
+            : Math.Round(number, Math.Clamp(3 * Math.Abs(lvlDiff), 0, 15));
+    }
+
+    static void Reset(this ref IdleNumber idleNumber)
+    {
+        idleNumber.Value = 0;
+        idleNumber.Lvl = 0;
+    }
+
+    public static string AsString(this IdleNumber number) => $"{(number.Value == 0 ? 0 : (Math.Truncate(number.Value * 100) / 100).ToString("###.##"))} {ConstantIdleNumber.LVL[number.Lvl]}";
+}
+}
---
+++ IdleNumber.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace IdleNumbers
+{
+[Serializable]
+public struct IdleNumber
+{
+    public double Value;
+    public int Lvl;
+
+    public IdleNumber(int value = 0, int lvl = 0, bool roundNumber = true) : this() => InitNumber(value, lvl, roundNumber);
+    public IdleNumber(float value = 0, int lvl = 0, bool roundNumber = true) : this() => InitNumber(value, lvl, roundNumber);
+    public IdleNumber(double value = 0, int lvl = 0, bool roundNumber = true) : this() => InitNumber(value, lvl, roundNumber);
+    public IdleNumber(IdleNumber targetIdleNumber, bool roundNumber = true) : this() => InitNumber(targetIdleNumber.Value, targetIdleNumber.Lvl, roundNumber);
+
+    void InitNumber(double value, int lvl, bool roundNumber)
+    {
+        Value = value;
+        Lvl = lvl;
+
+        if (!roundNumber) return;
+        this.CheckAndRoundIdleNumber();
+    }
+
+#region IdleNumber & IdleNumber Operators Override
+
+    public static IdleNumber operator +(IdleNumber idleNumberA, IdleNumber idleNumberB) => Extensions.Addition(idleNumberA, idleNumberB);
+    public static IdleNumber operator *(IdleNumber idleNumberA, IdleNumber idleNumberB) => Extensions.Multiply(idleNumberA, idleNumberB);
+    public static IdleNumber operator -(IdleNumber idleNumberA, IdleNumber idleNumberB) => Extensions.Subtract(idleNumberA, idleNumberB);
+    public static IdleNumber operator /(IdleNumber idleNumberA, IdleNumber idleNumberB) => Extensions.Divide(idleNumberA, idleNumberB);
+
+#endregion
+
+#region IdleNumber & int Operators Override
+
+    public static IdleNumber operator +(IdleNumber idleNumberA, int numberB) => idleNumberA + new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator *(IdleNumber idleNumberA, int numberB) => idleNumberA * new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator -(IdleNumber idleNumberA, int numberB) => idleNumberA - new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator /(IdleNumber idleNumberA, int numberB) => idleNumberA / new IdleNumber(numberB, 0, false);
+
+    public static IdleNumber operator +(int numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) + idleNumberB;
+    public static IdleNumber operator *(int numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) * idleNumberB;
+    public static IdleNumber operator -(int numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) - idleNumberB;
+    public static IdleNumber operator /(int numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) / idleNumberB;
+
+#endregion
+
+#region IdleNumber & float Operators Override
+
+    public static IdleNumber operator +(IdleNumber idleNumberA, float numberB) => idleNumberA + new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator *(IdleNumber idleNumberA, float numberB) => idleNumberA * new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator -(IdleNumber idleNumberA, float numberB) => idleNumberA - new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator /(IdleNumber idleNumberA, float numberB) => idleNumberA / new IdleNumber(numberB, 0, false);
+
+    public static IdleNumber operator +(float numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) + idleNumberB;
+    public static IdleNumber operator *(float numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) * idleNumberB;
+    public static IdleNumber operator -(float numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) - idleNumberB;
+    public static IdleNumber operator /(float numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) / idleNumberB;
+
+#endregion
+
+#region IdleNumber & double Operators Override
+
+    public static IdleNumber operator +(IdleNumber idleNumberA, double numberB) => idleNumberA + new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator *(IdleNumber idleNumberA, double numberB) => idleNumberA * new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator -(IdleNumber idleNumberA, double numberB) => idleNumberA - new IdleNumber(numberB, 0, false);
+    public static IdleNumber operator /(IdleNumber idleNumberA, double numberB) => idleNumberA / new IdleNumber(numberB, 0, false);
+
+    public static IdleNumber operator +(double numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) + idleNumberB;
+    public static IdleNumber operator *(double numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) * idleNumberB;
+    public static IdleNumber operator -(double numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) - idleNumberB;
+    public static IdleNumber operator /(double numberA, IdleNumber idleNumberB) => new IdleNumber(numberA, 0, false) / idleNumberB;
+
+#endregion
+
+}
+}
